be352d51568a815dbfbeba393d8b0aa78d860e66
Ally <alex.ogilvie@gmail.com>
2012-07-02 17:43:06 +0900

    added and renamed js

---
+++ www/phonegap/plugin/localNotification/localNotification.js
@@ -0,0 +1,62 @@
+/**
+ * 
+ * @author Ally Ogilvie
+ * @copyright WizCorp Inc. [ Incorporated Wizards ] 2011
+ * @file localNotification.m for PhoneGap
+ *
+ */
+
+var localNotification = {
+
+	add : function(id, options) {
+
+		return PhoneGap.exec(null, null, "LocalNotification", "addNotification", [id, options]);
+	},
+
+	cancel : function(id) {
+		return PhoneGap.exec(null, null, "LocalNotification", "cancelNotification", [id]);
+	},
+	
+	cancelAll : function() {
+        return PhoneGap.exec(null, null,"LocalNotification", "cancelAllNotifications", []);
+    },
+    
+    queue : function(id, options) {
+        
+		return PhoneGap.exec(null, null, "LocalNotification", "queueNotification", [id, options]);
+	}
+    
+
+}
+
+
+
+/*
+*
+*   Example functions
+*
+
+function sendNotif() {
+    myOptions = {
+        seconds: 30,
+        message: 'chaaaarrrliieeeee',
+        badge: 1
+    }
+    localNotification.add( "0", myOptions );
+    
+};
+
+function cancelLastNotif() {
+    localNotification.cancel( "0" );
+    
+};
+
+function cancelAllNotif() {
+    
+    localNotification.cancelAll();
+    
+};
+
+
+
+*/
