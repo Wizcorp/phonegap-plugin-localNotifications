48320c8feb289b36ef2c5fdc0c1f1ad3e1d7b8f2
aogilvie <aogilvie@wizcorp.jp>
2013-10-10 17:58:18 +0900

    hard added example files pre-setup. CLI not required for example

---
+++ example/ios/www/cordova_plugins.js
@@ -0,0 +1,11 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "phonegap/plugin/localNotification/localNotification.js",
+        "id": "jp.wizcorp.phonegap.plugin.localNotificationPlugin",
+        "clobbers": [
+            "window.localNotification"
+        ]
+    }
+]
+});
